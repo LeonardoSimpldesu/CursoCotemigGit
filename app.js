929f14b3f725104c1829f885ec0c94c760377f42
LEONARDO_DE_SOUZA_ALMEIDA <12001031@DELTAB10.BARROCA>
2022-08-13 10:56:56 -0300

    Edição app.js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 Console.log("Bom dia! Esse é o início do meu Código. Infelizmente não terá muita informação aqui, mas, fique a vontade de deixar seu recado! :3")
 
 Console.log("Sucesso")
+
+Console.log("Apenas...sucesso!?")
